08a1d3917c23a8fde512c8940434d83da07644ad
Wes Hulette <jwhulette@gmail.com>
2020-05-20 20:43:08 -0400

    SSL WS

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -19,9 +19,9 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+window.axios = require("axios");
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -29,16 +29,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-import Echo from 'laravel-echo';
+import Echo from "laravel-echo";
 
-window.Pusher = require('pusher-js');
+window.Pusher = require("pusher-js");
 
 window.Echo = new Echo({
-    broadcaster: 'pusher',
+    broadcaster: "pusher",
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
     wsPort: 6001,
     enableStats: false,
-    forceTLS: false,
-    enabledTransports: ['ws']
+    forceTLS: true,
+    encrypted: true,
+    enabledTransports: ["ws"]
 });
