279e0d0a35e074eae78057c7729fc14c58b22c8c
Wes Hulette <jwhulette@gmail.com>
2020-05-25 17:01:28 -0400

    ws

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -37,7 +37,7 @@ window.Echo = new Echo({
     broadcaster: "pusher",
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
-    wsPort: 6001,
+    wsPort: 443,
     disableStats: true,
     encrypted: true,
     forceTLS: false
