d447386388a65213103e61d42329360c90de0cc4
Wes Hulette <jwhulette@gmail.com>
2020-05-25 16:57:01 -0400

    ws logging

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -38,9 +38,9 @@ window.Echo = new Echo({
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
     wsPort: 6001,
-    wssHost: window.location.hostname,
-    wssPort: 6001,
     disableStats: true,
     encrypted: true,
     forceTLS: false
 });
+
+window.Echo.connector.pusher.connection.timeline.events;
