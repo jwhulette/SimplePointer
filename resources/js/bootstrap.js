98b640f438d7cd9a678794476f78fa46261dfe35
Wes Hulette <jwhulette@gmail.com>
2020-05-25 16:39:49 -0400

    ws

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -38,6 +38,8 @@ window.Echo = new Echo({
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
     wsPort: 6001,
+    wssHost: window.location.hostname,
+    wssPort: 6001,
     disableStats: true,
     encrypted: true,
     forceTLS: false
