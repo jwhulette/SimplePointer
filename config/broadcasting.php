770a822b7ff9d2a9d5bed5341f1b52a8f9ef50ee
Wes Hulette <jwhulette@gmail.com>
2020-05-25 16:39:49 -0400

    Fixed websocket issue

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,10 +37,9 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
                 'host' => env('PUHSER_HOST', '127.0.0.1'),
                 'port' => 6001,
-                'scheme' => 'https',
+                'scheme' => 'http',
             ],
         ],
 
