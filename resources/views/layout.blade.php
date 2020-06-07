1c14c20826c624021574633c5cd73c7d992dc19c
Wes Hulette <jwhulette@gmail.com>
2020-06-07 18:42:57 -0400

    Added google analytics

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,4 +1,13 @@
 <!DOCTYPE html>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-168725062-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-168725062-1');
+</script>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
