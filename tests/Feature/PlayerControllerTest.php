80351cda44d9e46fd5955e110d57c3568fc17b0c
Wes Hulette <jwhulette@gmail.com>
2020-08-27 19:14:36 -0400

    WIP

--- tests/Feature/PlayerControllerTest.php
+++ tests/Feature/PlayerControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Room;
 use App\User;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PlayerControllerTest extends TestCase
@@ -16,7 +17,7 @@ class PlayerControllerTest extends TestCase
      *
      * @return void
      */
-    public function test_create_new_player()
+    public function test_new_player_join()
     {
         $room = factory(Room::class)->create();
 
@@ -31,5 +32,36 @@ class PlayerControllerTest extends TestCase
         ]);
 
         $response->assertStatus(200);
+
+        $this->assertTrue(is_array($response['user']));
+
+        $this->assertTrue(Auth::check());
+    }
+
+    public function test_existing_player_join()
+    {
+        $room = factory(Room::class)->create();
+
+        $user = factory(User::class)->create([
+            'room_id' => $room->uuid,
+        ]);
+
+        $response = $this->put(route('join'), [
+            'userid' => $user->id,
+            'name' => $user->name,
+            'type' => 1,
+            'roomid' => $user->room_id,
+        ]);
+
+        $response->assertStatus(200);
+
+        $response->assertJsonFragment([
+            'id' => $user->id,
+            'name' => $user->name,
+            'type' => 1,
+            'room_id' => $user->room_id,
+        ]);
+
+        $this->assertTrue(Auth::check());
     }
 }
