c57e7d7e54cc19026676a31d81253718bf087997
Wes Hulette <jwhulette@gmail.com>
2020-10-25 13:04:26 -0400

    NPM updates

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const tailwindcss = require("tailwindcss");
  */
 
 mix.js("resources/js/app.js", "public/js")
+    .vue()
     .sass("resources/sass/app.scss", "public/css")
     .options({
         processCssUrls: false,
