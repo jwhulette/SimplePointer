137f156e89b84fbd0fb999f3c0cc2bc643f449eb
Wes Hulette <jwhulette@gmail.com>
2020-12-27 15:14:27 -0500

    Updated composer packages

---
+++ app/Console/Commands/PhpstanCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+
+class PhpstanCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'phpstan';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run phpstan';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $phpstan = new Process(["composer", "phpstan"]);
+
+        $phpstan->run();
+
+        echo $phpstan->getOutput();
+
+        return 0;
+    }
+}
